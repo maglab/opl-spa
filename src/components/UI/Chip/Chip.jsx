8962e09cff981fb8293f52c93c6fc02914d5c302
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-12-05 17:51:53 +0000

    Changing chip layout.

--- src/components/UI/Chip/Chip.jsx
+++ src/components/UI/Chip/Chip.jsx
@@ -1,9 +1,9 @@
-function Chip(props){
-    return(
-        <div className="chip border border-theme-blue w-fit rounded-3xl px-2 bg-theme-blue text-white">
-            {props.children}
-        </div>
-    )
-}; 
+function Chip({ children }) {
+  return (
+    <div className="chip border border-theme-blue w-fit rounded-3xl px-2 bg-white text-xs hover:bg-theme-blue hover:text-white">
+      {children}
+    </div>
+  );
+}
 
-export default Chip; 
+export default Chip;
