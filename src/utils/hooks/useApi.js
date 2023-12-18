26030f0e9db472f57c31071329d443b9d228a3ba
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-12-18 17:05:12 +0000

    Expanded hook to include new method of get requests with parameters

--- src/utils/hooks/useApi.js
+++ src/utils/hooks/useApi.js
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
-
+import { apiRequest } from "../../api/apiClient";
 // To replace useApi
-function useGetApi(api, params) {
+function useGetApi(apiFunction, params) {
   const [apiData, setApiData] = useState(null);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
     async function getApiData() {
+      setLoading(true);
       try {
-        const response = await api(params);
-        const data = response.data;
-        setApiData(data);
-      } catch (error) {
-        setError(error);
+        // Call the provided API function with params
+        const response = await apiFunction(params);
+        // Check if the response is an error instance
+        if (response instanceof Error) {
+          setError(response);
+        } else {
+          setApiData(response.data);
+        }
+      } catch (err) {
+        setError(err);
       } finally {
         setLoading(false);
       }
     }
+
     getApiData();
-  }, [params]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [apiFunction, JSON.stringify(params)]); // adding apiFunction and params as dependencies
+
   return { isLoading, apiData, error };
 }
 
