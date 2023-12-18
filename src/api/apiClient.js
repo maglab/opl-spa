57efeb43c62a3385c29a3ee2f99f517748284acb
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-12-18 17:04:46 +0000

    Added a request function to remove unecessary try, catch request calls from other api modules.

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,11 +3,20 @@ import { get_web_api_url } from "../config";
 
 // Create an instance of Axios with default configuration
 const apiClient = axios.create({
-    baseURL: get_web_api_url(document.location), // Replace with API base URL
-    timeout: 10000, // Request timeout in milliseconds
-    headers: {
-      "Content-Type": "application/json", // Set appropriate headers if needed
-    },
-  });
+  baseURL: get_web_api_url(document.location), // Replace with API base URL
+  timeout: 10000, // Request timeout in milliseconds
+  headers: {
+    "Content-Type": "application/json", // Set appropriate headers if needed
+  },
+});
 
-export default apiClient; 
+export async function apiRequest(callback) {
+  try {
+    const response = await callback();
+    return response;
+  } catch (error) {
+    return error;
+  }
+}
+
+export default apiClient;
