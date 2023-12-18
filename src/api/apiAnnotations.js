2e68b8f282b2c9898a4e876ea60c77c2bd58cb04
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-12-18 17:06:27 +0000

    Expanded getAnnotationsForProblem api call to allow for queries.

--- src/api/apiAnnotations.js
+++ src/api/apiAnnotations.js
@@ -1,73 +1,83 @@
 import apiClient from "./apiClient";
+import { apiRequest } from "./apiClient";
 
+/**
+ * Retrieves details of a specific annotation.
+ * @param {Object} params - Parameters for the API request.
+ * @param {string} params.annotation - The type of annotation.
+ * @param {string} params.annotationId - The ID of the annotation.
+ * @returns {Promise} A promise that resolves with the annotation details.
+ */
 const apiAnnotations = {
   getAnnotationDetails: async (params) => {
-    //Get request for getting a single annotation and its details from the api
-    const annotation = params.annotation;
-    const annotationId = params.annotationId;
-    try {
-      const response = await apiClient.get(
-        `annotations/${annotation}/${annotationId}`
-      );
-      return response;
-    } catch (error) {
-      return error;
-    }
+    const { annotation, annotationId } = params;
+    return apiRequest(() =>
+      apiClient.get(`annotations/${annotation}/${annotationId}`)
+    );
   },
-  getAnnotationsForProblem: async (params) => {
-    //Get request for getting annotations of a particular type (eg. Gene) for a problem
-    const annotation = params.annotation;
-    const problemId = params.problemId;
 
-    try {
-      const response = await apiClient.get(
-        `annotations/${annotation}/filter/by-problem:${problemId}`
-      );
-      return response;
-    } catch (error) {
-      return error;
-    }
+  /**
+   * Retrieves annotations for a given problem.
+   * @param {Object} params - Parameters for the API request.
+   * @param {string} params.annotation - The type of annotation.
+   * @param {string|null} params.problemId - The ID of the problem (defaults to null).
+   * @param {boolean} params.all - Flag to retrieve all annotations (defaults to false).
+   * @returns {Promise} A promise that resolves with the annotations.
+   */
+  getAnnotationsForProblem: async (params) => {
+    const {
+      annotation,
+      problemId = null,
+      all = false,
+      fields = false,
+    } = params;
+    const queryParams = fields.length > 0 ? { fields: fields.join(",") } : {};
+    console.log(queryParams);
+
+    const path = all
+      ? `annotations/all/${problemId}`
+      : `annotations/${annotation}/filter/by-problem:${problemId}`;
+
+    return apiRequest(() => apiClient.get(path, { params: queryParams }));
   },
+
+  /**
+   * Retrieves problems associated with a specific annotation type.
+   * @param {Object} params - Parameters for the API request.
+   * @param {string} params.annotation - The type of annotation.
+   * @param {string} params.annotationId - The ID of the annotation.
+   * @returns {Promise} A promise that resolves with the problems.
+   */
   getProblemsForAnnotation: async (params) => {
-    // Get request for getting all problems for a particular annotation type
-    const annotation = params.annotation;
-    const annotationId = params.annotationId;
-    try {
-      const response = await apiClient.get(
+    const { annotation, annotationId } = params;
+    return apiRequest(() =>
+      apiClient.get(
         `annotations/${annotation}/filter/by-annotation:${annotationId}`
-      );
-      return response;
-    } catch (error) {
-      return error;
-    }
+      )
+    );
   },
+
+  /**
+   * Retrieves all entries for a given annotation type.
+   * @param {Object} params - Parameters for the API request.
+   * @param {string} params.annotation - The type of annotation.
+   * @returns {Promise} A promise that resolves with the annotation entries.
+   */
   getAnnotationEntries: async (params) => {
-    const annotation = params.annotation;
-    try {
-      const response = await apiClient.get(`annotations/${annotation}/`);
-      return response;
-    } catch (error) {
-      return error;
-    }
+    const { annotation } = params;
+    return apiRequest(() => apiClient.get(`annotations/${annotation}/`));
   },
+
+  /**
+   * Sends filter parameters to the API and retrieves filtered annotations.
+   * @param {Object} params - Parameters for the API request.
+   * @param {Object} params.filters - The filter parameters.
+   * @returns {Promise} A promise that resolves with the filtered annotations.
+   */
   sendFilters: async (params) => {
-    const filters = params.filters;
-    try {
-      const response = await apiClient.post("annotations/filter", filters);
-      return response;
-    } catch (error) {
-      return error;
-    }
+    const { filters } = params;
+    return apiRequest(() => apiClient.post("annotations/filter", filters));
   },
-  // applyFilters: async (params) => {
-  //   const queryUrl = params.queryUrl;
-  //   try {
-  //     const response = await apiClient.get(`${queryUrl}`);
-  //     return response;
-  //   } catch (error) {
-  //     return error;
-  //   }
-  // },
 };
 
 export default apiAnnotations;
