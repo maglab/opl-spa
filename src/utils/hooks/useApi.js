c365df5ffd40666875d61c980d9f41bdfe23adda
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-01-26 21:40:45 +0000

    NA

--- src/utils/hooks/useApi.js
+++ src/utils/hooks/useApi.js
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
  * Hook for making an API call and setting a loading and error state for conditional rendering.
  * @param {Function} apiFunction - Api call function.
  * @param {Object} params - Used to set the query parameters for the get request.
+ * @param {*} defaultApi - Sets the default value for the API data state
  * @returns {Object} - Returns a state for the data, loading and error.
  */
-function useGetApi(apiFunction, params) {
-  const [apiData, setApiData] = useState(null);
+function useGetApi(apiFunction, params, defaultApi = null) {
+  const [apiData, setApiData] = useState(defaultApi);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -17,12 +18,7 @@ function useGetApi(apiFunction, params) {
       try {
         // Call the provided API function with params
         const response = await apiFunction(params);
-        // Check if the response is an error instance
-        if (response instanceof Error) {
-          setError(response);
-        } else {
-          setApiData(response.data);
-        }
+        setApiData(response.data);
       } catch (error) {
         setError(error);
       } finally {
@@ -31,7 +27,7 @@ function useGetApi(apiFunction, params) {
     }
 
     getApiData();
-  }, [JSON.stringify(params)]); // adding apiFunction and params as dependencies
+  }, [JSON.stringify(params)]);
 
   return { isLoading, apiData, error };
 }
