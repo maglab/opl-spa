25a64bb073058553888877b01088fe246a79f628
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-01-26 15:59:48 +0000

    Removed apiFunction dependancy which was causing infinite loop.

--- src/utils/hooks/useApi.js
+++ src/utils/hooks/useApi.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-// To replace useApi
+
+/**
+ * Hook for making an API call and setting a loading and error state for conditional rendering.
+ * @param {Function} apiFunction - Api call function.
+ * @param {Object} params - Used to set the query parameters for the get request.
+ * @returns {Object} - Returns a state for the data, loading and error.
+ */
 function useGetApi(apiFunction, params) {
   const [apiData, setApiData] = useState(null);
   const [isLoading, setLoading] = useState(true);
@@ -25,7 +31,7 @@ function useGetApi(apiFunction, params) {
     }
 
     getApiData();
-  }, [apiFunction, JSON.stringify(params)]); // adding apiFunction and params as dependencies
+  }, [JSON.stringify(params)]); // adding apiFunction and params as dependencies
 
   return { isLoading, apiData, error };
 }
