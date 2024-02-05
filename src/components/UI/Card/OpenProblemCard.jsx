1ddba63c38275cf5f45e07a192f47b7b6cd172dd
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-05 15:08:52 +0000

    Added skeleton for cards

--- src/components/UI/Card/OpenProblemCard.jsx
+++ src/components/UI/Card/OpenProblemCard.jsx
@@ -1,10 +1,10 @@
 function OpenProblemCard({ title, description }) {
   return (
-    <div className="card bg-white">
+    <div className="card bg-white shadow-md p-4 rounded">
       <span>
-        <h1> {title}</h1>
+        <h1 className="text-theme-blue"> {title}</h1>
       </span>
-      <span>{description}</span>
+      <span className="text-sm">{description}</span>
     </div>
   );
 }
