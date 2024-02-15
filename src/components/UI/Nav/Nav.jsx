c3a489c8ab69a8b1382eae433622a3aae276305e
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 12:47:41 +0000

    Update Nav.jsx

--- src/components/UI/Nav/Nav.jsx
+++ src/components/UI/Nav/Nav.jsx
@@ -1,11 +1,11 @@
 import NavMenu from "./NavMenu";
 
-export const Nav2 = () => {
+export function Nav2() {
   return (
     <nav id="nav" className="nav bg-white shadow-md flex justify-center w-full">
       <NavMenu />
     </nav>
   );
-};
+}
 
 // export default Nav;
