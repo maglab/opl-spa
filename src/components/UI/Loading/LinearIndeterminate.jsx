ace06543bc52bc5482b4cc00038d8698dc6d74b2
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 12:45:56 +0000

    Update LinearIndeterminate.jsx

--- src/components/UI/Loading/LinearIndeterminate.jsx
+++ src/components/UI/Loading/LinearIndeterminate.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const IndeterminateLinearProgress = () => {
+function IndeterminateLinearProgress() {
   return (
     <div className="w-full bg-gray-200 rounded-full overflow-hidden h-2.5 dark:bg-gray-700">
       <div
         className="bg-blue-600 h-2.5 rounded-full animate-progress"
         style={{ width: "50%" }}
-      ></div>
+      />
     </div>
   );
-};
+}
 
 export default IndeterminateLinearProgress;
