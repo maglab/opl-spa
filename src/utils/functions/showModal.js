e605a030363291ab1cf49eae10a73ffeade27938
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 12:38:18 +0000

    Update showModal.js

--- src/utils/functions/showModal.js
+++ src/utils/functions/showModal.js
@@ -1,12 +1,13 @@
 import { generalActions } from "../../state/generalStateSlice";
-const showModal = (dispatch, title, message, status) => {
-    const modalValue = {
-      title,
-      message,
-      status,
-    };
-    dispatch(generalActions.toggleModal());
-    dispatch(generalActions.setModalContent({ content: modalValue }));
-  };
 
-  export default showModal;
+const showModal = (dispatch, title, message, status) => {
+  const modalValue = {
+    title,
+    message,
+    status,
+  };
+  dispatch(generalActions.toggleModal());
+  dispatch(generalActions.setModalContent({ content: modalValue }));
+};
+
+export default showModal;
