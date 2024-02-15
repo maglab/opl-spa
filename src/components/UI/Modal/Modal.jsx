c8759ff3575f3a21f48c820de3466d8caa8c4921
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 12:04:20 +0000

    Update Modal.jsx
    MUI Modal implementation

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
-
-function Modal({
+import { Modal, Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { generalActions } from "../../../state/generalStateSlice";
+function ModalT({
   open,
   close,
   height,
@@ -37,4 +39,40 @@ function Modal({
   );
 }
 
-export default Modal;
+export function SubmissionModal({ title, response }) {
+  const modalOpen = useSelector((state) => state.general.modal.isOpen);
+  const dispatch = useDispatch();
+  const closeHandler = () => {
+    dispatch(generalActions.toggleModal({ bool: false }));
+  };
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={closeHandler}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white max-h-[250px] max-w-[350px] w-full h-full p-4 flex flex-col justify-center gap-y-6 items-center ">
+        <div className="modal-text w-full">
+          <h1
+            id="modal-title"
+            className="text-center font-general text-lg md:text-2xl py-2"
+          >
+            {title}
+          </h1>
+          <p
+            id="modal-description"
+            className="text-center font-general text-sm md:text-base"
+          >
+            {response}
+          </p>
+        </div>
+        <div className="modal-buttons">
+          <Button onClick={closeHandler}>Exit</Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ModalT;
