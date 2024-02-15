62e349a793589b45b28a504b69f01617159a188a
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 13:21:37 +0000

    Update OpenProblems.jsx

--- src/pages/OpenProblems/OpenProblems.jsx
+++ src/pages/OpenProblems/OpenProblems.jsx
@@ -1,33 +1,32 @@
-import { useSelector, useDispatch } from "react-redux";
+import { Container } from "@mui/material";
+import { useDispatch } from "react-redux";
 import { generalActions } from "../../state/generalStateSlice";
-import Statbar from "./Statbar/Statbar";
-import SearchBar from "./SearchBar/SearchBar";
-import ProblemsInterface from "./ProblemsInterface/ProblemsInterface";
-import Modal from "../../components/UI/Modal/Modal";
-import Filter from "./Filter/Filter";
 import config from "../../utils/configs/SideNavConfig";
+import Filter from "./Filter/Filter";
+import ProblemsInterface from "./ProblemsInterface/ProblemsInterface";
+import SearchBar from "./SearchBar/SearchBar";
+import Statbar from "./Statbar/Statbar";
 
 function OpenProblems() {
-  const modalState = useSelector((state) => state.question.modalOpen);
-  const questionDetails = useSelector((state) => state.question.modalDetails);
   const dispatch = useDispatch();
   const modalCloseHandler = () => {
     dispatch(generalActions.toggleModal({ bool: false }));
   };
 
   return (
-    <div className="w-full flex flex-row pt-6">
-      {/* Filter Side Navigation */}
-      <div className="side-nav w-1/5 sticky">
-        <Filter config={config} />
+    <Container>
+      <div className="w-full flex flex-row pt-6">
+        <div className="side-nav w-1/5 sticky">
+          <Filter config={config} />
+        </div>
+        {/* Main Content */}
+        <div className="main w-full h-max overflow-auto">
+          <SearchBar label="Search for an open problem" />
+          <Statbar className="statbar" />
+          <ProblemsInterface />
+        </div>
       </div>
-      {/* Main Content */}
-      <div className="main w-full h-max overflow-auto">
-        <SearchBar label="Search for an open problem" />
-        <Statbar className="statbar" />
-        <ProblemsInterface />
-      </div>
-    </div>
+    </Container>
   );
 }
 
