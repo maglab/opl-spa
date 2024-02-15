e893862769d727af19c50100815bdf18d6ab9b69
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 13:21:26 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import OpenProblems from "./pages/OpenProblems/OpenProblems";
-import SubmitPage from "./pages/Submit/Submit";
-import apiProblems from "./api/apiProblems";
-import Details from "./pages/OpenProblemDetails/Details";
-import AnnotationDetails from "./pages/AnnotationDetails/AnnotationDetails";
-=======
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { blue, grey } from "@mui/material/colors";
 import React from "react";
 import { RouterProvider } from "react-router-dom";
->>>>>>> development
 import "./index.css";
 import router from "./routes/router";
 
