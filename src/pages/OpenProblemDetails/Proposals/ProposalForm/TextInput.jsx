7d92c3e8cd15bcd3fd5ab0267d75899edcad24f7
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-15 12:51:45 +0000

    Update TextInput.jsx

--- src/pages/OpenProblemDetails/Proposals/ProposalForm/TextInput.jsx
+++ src/pages/OpenProblemDetails/Proposals/ProposalForm/TextInput.jsx
@@ -1,13 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { detailsActions } from "../../../../state/Details/detailsSlice";
+
 function TextInput() {
   const textState = useSelector(
-    (state) => state.details.submission.description
+    (state) => state.details.submission.description,
   );
   const dispatch = useDispatch();
   const descriptionHandler = (e) => {
-    const value = e.target.value;
-    dispatch(detailsActions.setFormValue({ id: "description", value: value }));
+    const { value } = e.target;
+    dispatch(detailsActions.setFormValue({ id: "description", value }));
   };
   return (
     <div className="m-auto flex w-9/12 flex-col px-6 py-2 pb-8 text-sm">
@@ -16,7 +17,7 @@ function TextInput() {
         value={textState}
         id="description"
         className="h-32 border border-theme-blue bg-bg-grey p-2"
-      ></textarea>
+      />
     </div>
   );
 }
