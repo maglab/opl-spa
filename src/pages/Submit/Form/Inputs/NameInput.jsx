2b02d56997cf6f73601831ce3cc3e25155556637
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-16 12:54:20 +0000

    Update NameInput.jsx
    MUI component

--- src/pages/Submit/Form/Inputs/NameInput.jsx
+++ src/pages/Submit/Form/Inputs/NameInput.jsx
@@ -1,43 +1,42 @@
+import { Box } from "@mui/material";
 import { useField } from "formik";
 
-import useViewWidth from "../../../../utils/hooks/useViewWidth";
+import { InputWithFormLabel } from "../../../../components/UI/Inputs/TextInput";
+
+const nameBoxStyles = {
+  display: "flex",
+  flexDirection: "row",
+  justifyItems: "evenly",
+  gap: "1rem",
+  width: "100%",
+};
+
 /**
  * Custom formik input for first name and last name.
  * @param {String} classNames - Additional classnames for tailwind css styling. Mainly for setting margin and padding.
  * @returns {React.Component}
  */
 
-export function NameInput({ paddingY }) {
-  const { isMobile } = useViewWidth();
+function NameInput() {
   const [fieldFirst, metaFirst] = useField("firstName", "text");
   const [fieldLast, metaLast] = useField("lastName", "text");
-
   return (
-    <fieldset
-      className={`w-full grid items-center py-${paddingY} ${
-        isMobile ? "grid-cols-1" : "grid-cols-[20%_80%]"
-      }`}
-    >
-      <label className="text-center font-semibold">Name</label>
-      <div className="flex flex-row gap-x-4 ">
-        <input
-          {...fieldFirst}
+    <Box>
+      <Box classNames="name-box" sx={nameBoxStyles}>
+        <InputWithFormLabel
           name="first_name"
-          type="text"
-          className="text-input h-fit-content h-auto w-full rounded-md border border-slate-500 bg-bg-grey p-2"
-          placeholder="First name"
+          label="First name"
+          field={fieldFirst}
+          meta={metaFirst}
         />
-        <input
-          {...fieldLast}
+        <InputWithFormLabel
           name="last_name"
-          type="text"
-          className="text-input h-fit-content h-auto w-full rounded-md border border-slate-500 bg-bg-grey p-2"
-          placeholder="Last name"
+          label="Last name"
+          field={fieldLast}
+          meta={metaLast}
         />
-      </div>
-      {metaFirst.touched && metaFirst.error && <div>{metaLast.error}</div>}
-      {metaLast.touched && metaLast.error && <div>{metaLast.error}</div>}
-    </fieldset>
+      </Box>
+    </Box>
   );
 }
 
