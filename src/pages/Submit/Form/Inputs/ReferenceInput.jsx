0a12c887ee7ce06ab8f59ad0605a2c81761996e4
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-16 12:03:22 +0000

    Update ReferenceInput.jsx
    MUI conversion of components

--- src/pages/Submit/Form/Inputs/ReferenceInput.jsx
+++ src/pages/Submit/Form/Inputs/ReferenceInput.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
 import { useField } from "formik";
+import React, { useEffect, useState } from "react";
 
-import useViewWidth from "../../../../utils/hooks/useViewWidth";
+import { Box, List, ListItem, ListItemText, Typography } from "@mui/material";
 import apiReferences from "../../../../api/apiReferences";
+import { InputWithFormLabelMultiline } from "../../../../components/UI/Inputs/TextArea";
+import useViewWidth from "../../../../utils/hooks/useViewWidth";
 import regexTest from "../functions/referenceRegex";
+
 /**
  * Formats inputted DOIs and PMIDs suitable for api call to verify
  * @param {Array} - Array of DOIs and PMIDs.
@@ -20,6 +23,25 @@ function formatReferences(inputValues) {
   return formattedRefs;
 }
 
+const validationBoxStyles = {
+  textAlign: "center",
+};
+const listItemTypography = {
+  color: "error",
+};
+const listStyles = {
+  paddingTop: 0,
+  paddingBottom: 0,
+  textAlign: "center",
+};
+const listItemStyle = {
+  width: "full",
+  display: "list-item",
+  paddingLeft: 0,
+  paddingTop: 0, // Override top padding
+  paddingBottom: 0, // Override bottom padding
+  textAlign: "center",
+};
 /**
  * Validation component handles input validation logic. Only validates correct prefix and pmid/doi value through regex.
  * @param {String} inputValue - DOI/PMID references to be validated.
@@ -54,23 +76,32 @@ function ValidationComponent({ inputValue, validUseState, invalidUseState }) {
     setInvalidReferences(failedTests);
   }, [inputValue]);
   return (
-    <div>
-      <p className="text-center">
-        DOI/PMID values must be prefixed with DOI: or PMID:
-      </p>
+    <Box className="validation-box" sx={validationBoxStyles}>
+      <Typography variant="subtitle1">
+        DOI/PMID values must be prefixed with DOI: or PMID.
+      </Typography>
       {invalidReferences.length > 0 && (
         <>
-          <h3 className="text-center text-red-600">
+          <Typography color="error">
             Incorrectly formatted references:
-          </h3>
-          <ul className="text-red-600">
-            {invalidReferences.map((ref) => (
-              <li key={ref}>{ref}</li>
+          </Typography>
+          <List className="list" sx={listStyles}>
+            {invalidReferences.map((reference) => (
+              <ListItem
+                key={reference}
+                className="list-item"
+                sx={listItemStyle}
+              >
+                <ListItemText
+                  primaryTypographyProps={listItemTypography}
+                  primary={reference}
+                />
+              </ListItem>
             ))}
-          </ul>
+          </List>
         </>
       )}
-    </div>
+    </Box>
   );
 }
 
@@ -79,7 +110,7 @@ function ValidationComponent({ inputValue, validUseState, invalidUseState }) {
  * @param {[String]} references - Array of references to be rendered
  * @returns
  */
-function ReferenceList({ classNames, references, error }) {
+function ReferenceList({ references, error }) {
   const {
     verified_references: verifiedReferences,
     unverified_references: unverifiedReferences,
@@ -87,44 +118,52 @@ function ReferenceList({ classNames, references, error }) {
   // Render the list of valid and invalid references here
   if (error) {
     return (
-      <div>
-        <p>Error retrieving references: {error.message} </p>
-      </div>
+      <Box>
+        <Typography variant="subtitle1" color="error">
+          Error retrieving references: {error.message}
+        </Typography>
+      </Box>
     );
   }
   return (
-    <div className={`${classNames}`}>
-      <div className="verified-refs flex flex-col">
+    <Box>
+      <Box className="verified-refs">
         {verifiedReferences.length > 0 && (
-          <p className="text-center"> Retrieved references: </p>
+          <Typography variant="body1">Retrieved references:</Typography>
         )}
-        <ul className="text-sm">
-          {verifiedReferences.map((reference) => (
-            <li key={reference.citation}>{reference.citation}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="unverified-refs flex flex-col">
+        {
+          <List>
+            {verifiedReferences.map((reference) => (
+              <ListItem key={reference.title}>{reference.citation}</ListItem>
+            ))}
+          </List>
+        }
+      </Box>
+      <Box className="unverified-refs">
         {unverifiedReferences.length > 0 && (
-          <p>Unable to retrieve reference information for:</p>
+          <Typography variant="body1">
+            Unable to retrieve reference information for:
+          </Typography>
         )}
-        <ul>
+        <List>
           {unverifiedReferences.map((reference) => (
-            <li>
-              {reference.type}:{reference.value} - {reference.error}
-            </li>
+            <ListItem key={reference.title}>{reference.citation}</ListItem>
           ))}
-        </ul>
-      </div>
-    </div>
+        </List>
+      </Box>
+    </Box>
   );
 }
+
+const boxStyles = {
+  width: "100%",
+};
 
 /**
  * Text area for references for submitting open problems. Validates inputted DOIs and PMIDs and then uses API to search existance of articles.
  * @returns {React.Component}
  */
-function ReferenceInput({ id, name, label, placeHolder, type }) {
+function ReferenceInput({ id, name, placeHolder, type }) {
   //Tracking input values
   const [field, meta] = useField(name, type);
   const { value } = meta;
@@ -147,6 +186,7 @@ function ReferenceInput({ id, name, label, placeHolder, type }) {
         const { data } = await apiReferences.verifyReferences({
           references: formattedReferences,
         });
+        console.log(data);
         setRetrievedReferences(data);
       } catch (error) {
         setError(error);
@@ -154,43 +194,29 @@ function ReferenceInput({ id, name, label, placeHolder, type }) {
     }, 1200);
 
     return () => clearTimeout(delayDebounceFunction);
-  }, [value]);
+  }, [value.split(",").length]);
 
   return (
-    <>
-      <div
-        className={`grid py-4 items-center ${
-          isMobile ? "grid-cols-1" : "grid-cols-[20%_80%]"
-        } text-center font-general`}
-      >
-        <label className="font-semibold" htmlFor={id}>
-          {label}
-        </label>
-        <div>
-          <ValidationComponent
-            inputValue={value}
-            validUseState={{ validReferences, setValidReferences }}
-            invalidUseState={{ invalidReferences, setInvalidReferences }}
-          />
-          <textarea
-            rows={2}
-            name={name}
-            type={type}
-            placeholder={placeHolder}
-            {...field}
-            className="text-input h-fit-content h-auto w-full rounded-md border border-slate-500 bg-bg-grey p-2"
-          />
-          {meta.touched && meta.error && (
-            <p className="text-red-500 ">{meta.error}</p>
-          )}
-          <ReferenceList
-            references={retrievedReferences}
-            error={error}
-            classNames="flex justify-center text-center"
-          />
-        </div>
-      </div>
-    </>
+    <Box sx={boxStyles}>
+      <InputWithFormLabelMultiline
+        id={id}
+        label="References (comma separated DOI/PMID)"
+        name={name}
+        field={field}
+        meta={meta}
+        placeHolder={placeHolder}
+      />
+      <ValidationComponent
+        inputValue={value}
+        validUseState={{ validReferences, setValidReferences }}
+        invalidUseState={{ invalidReferences, setInvalidReferences }}
+      />
+      <ReferenceList
+        references={retrievedReferences}
+        error={error}
+        classNames="flex justify-center text-center"
+      />
+    </Box>
   );
 }
 
