ef1c6570ee80d50866e39e3fa812545cd55cff71
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-16 09:44:16 +0000

    Add MUI Text Area

--- src/components/UI/Inputs/TextArea.jsx
+++ src/components/UI/Inputs/TextArea.jsx
@@ -1,4 +1,43 @@
+import { Box, FilledInput, FormHelperText, FormLabel } from "@mui/material";
+import { useField } from "formik";
 import withOnChangeHandler from "../../../utils/hoc/withOnChangeHandler";
+
+const boxStyles = {
+  alignItems: "center",
+  paddingTop: "2rem",
+  paddingBottom: "2rem",
+};
+
+const labelStyles = {
+  fontSize: 18,
+};
+export function InputWithFormLabelMultiline({
+  label,
+  onChange,
+  onBlur,
+  error,
+  name,
+  required,
+}) {
+  const [field, meta] = useField(name, "textarea");
+
+  return (
+    <Box sx={boxStyles}>
+      <FormLabel sx={labelStyles}>{label}</FormLabel>
+      <FilledInput
+        fullWidth
+        name={name}
+        onChange={field.onChange}
+        onBlur={field.onBlur}
+        error={meta.error}
+        required={required}
+        multiline={true}
+        minRows={3}
+      />
+      <FormHelperText>{meta.error}</FormHelperText>
+    </Box>
+  );
+}
 
 function textArea(props) {
   return (
