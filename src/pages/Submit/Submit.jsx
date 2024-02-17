26840a52a98e104f01493133f596bde8a1574541
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-17 11:41:55 +0000

    Update Submit.jsx

--- src/pages/Submit/Submit.jsx
+++ src/pages/Submit/Submit.jsx
@@ -18,34 +18,35 @@ const typographyStyles = {
   paddingBottom: "16px",
 };
 
-const openProblemsBoxStyles = {
-  paddingTop: "2rem",
-  paddingBottom: "4rem",
-};
-
 function SubmitPage() {
   const [modalOpen, setModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(INITIAL_STATE);
   return (
-    <Container>
-      <Box className="header" flex alignItems="center">
-        <Typography {...typographyStyles}>Submit an open problem</Typography>
-        <Guidance />
-        <Divider aria-hidden={true} />
-      </Box>
-      <Box className="open-problems-box" {...openProblemsBoxStyles}>
-        <OpenProblemForm
-          setModalOpen={setModalOpen}
-          setModalContent={setModalContent}
-        />
-      </Box>
-
+    <>
+      <Container className="submit-page-container">
+        <Box className="header" flex alignItems="center">
+          <Typography {...typographyStyles}>Submit an open problem</Typography>
+          <Guidance />
+          <Divider aria-hidden={true} />
+        </Box>
+        <Box
+          className="open-problems-box"
+          paddingTop="2rem"
+          paddingBottom="4rem"
+        >
+          <OpenProblemForm
+            setModalOpen={setModalOpen}
+            setModalContent={setModalContent}
+          />
+        </Box>
+      </Container>
       <SubmissionModal
         open={modalOpen}
         title={modalContent.title}
         response={modalContent.response}
+        setOpen={setModalOpen}
       />
-    </Container>
+    </>
   );
 }
 export default SubmitPage;
