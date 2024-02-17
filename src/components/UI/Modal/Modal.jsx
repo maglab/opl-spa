332d2b5fbf29998bb92aab21ba0eadd427f133a8
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-17 11:41:34 +0000

    Update Modal.jsx
    Modal refactoring

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -4,30 +4,44 @@ import { Box, Button, Modal, Typography } from "@mui/material";
 const modalBoxStyling = {
   position: "absolute",
   top: "50%",
-  left: "-50%",
-  transform: "translateY(-50%)",
-  transform: "translateX(-50%)",
-  bgColor: "primary.main",
-  maxHeight: "250px",
-  width: "100%",
-  height: "100%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "primary.main",
   padding: "16px",
+  minHeight: "250px",
+  minWidth: "300px",
+  maxHeight: "25%",
+  maxWidth: "30%",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  gap: "1.5rem",
   alignItems: "center",
+  gap: "2rem",
+};
+const modalStyling = {
+  height: "100%",
+  width: "100%",
 };
 export function SubmissionModal({ title, response, open, setOpen }) {
+  const exitHandler = () => {
+    setOpen(false);
+  };
   return (
     <Modal
       open={open}
+      onClose={exitHandler}
       aria-labelledby="modal-title"
       aria-describedby="modal-description"
+      sx={modalStyling}
     >
       <Box className="modal-box" sx={modalBoxStyling}>
-        <Box className="modal-text w-full">
-          <Typography id="modal-title" variant="h5">
+        <Box className="modal-text" textAlign="center">
+          <Typography
+            id="modal-title"
+            variant="h5"
+            paddingTop="0.5rem"
+            paddingBottom="0.5rem"
+          >
             {title}
           </Typography>
           <Typography variant="body1" id="modal-description">
@@ -35,7 +49,9 @@ export function SubmissionModal({ title, response, open, setOpen }) {
           </Typography>
         </Box>
         <Box className="modal-buttons">
-          <Button>Exit</Button>
+          <Button color="secondary" variant="outlined" onClick={exitHandler}>
+            Exit
+          </Button>
         </Box>
       </Box>
     </Modal>
