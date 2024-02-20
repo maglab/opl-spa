67fe3b3f75d730e5ad08b07e2b99550467f70ecd
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-20 18:56:36 +0000

    Update OpenProblems.jsx

--- src/pages/OpenProblems/OpenProblems.jsx
+++ src/pages/OpenProblems/OpenProblems.jsx
@@ -1,43 +1,45 @@
-import { useSelector, useDispatch } from "react-redux";
-import { generalActions } from "../../state/generalStateSlice";
-import SubmissionModalContent from "../../components/UI/Modal/SubmissionModalContent";
-import Statbar from "./Statbar/Statbar";
-import SearchBar from "./SearchBar/SearchBar";
-import ProblemsInterface from "./ProblemsInterface/ProblemsInterface";
-import Modal from "../../components/UI/Modal/Modal";
-import Filter from "./Filter/Filter";
-import config from "../../utils/configs/SideNavConfig";
+import React, { useContext, useState, useEffect } from "react";
+import { Stack } from "@mui/material";
+
+import { OpenProblemsContext } from "../../context/context";
+import OpenProblemList from "./list";
+import Header from "./header";
+import apiProblems from "../../api/apiProblems";
 
 function OpenProblems() {
-  const modalState = useSelector((state) => state.question.modalOpen);
-  const questionDetails = useSelector((state) => state.question.modalDetails);
-  const dispatch = useDispatch();
-  const modalCloseHandler = () => {
-    dispatch(generalActions.toggleModal({ bool: false }));
-  };
+  const { state, dispatch } = useContext(OpenProblemsContext);
+  const { query, page, sorting, view } = state;
+  const [loading, setLoading] = useState(true);
+  const [apiError, setError] = useState("");
+  const [openProblems, setOpenProblems] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const { data } = await apiProblems.getProblems({
+          queryParams: {
+            p: page,
+            sorting,
+            search: query,
+            page_size: view === "list" ? 20 : 10,
+          },
+        });
+        setOpenProblems(data.results && data.results);
+        dispatch({ type: "setCount", payload: { count: data.count } });
+        dispatch({ type: "" });
+      } catch (error) {
+        setError(apiError);
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, [query, page, sorting, view]);
 
   return (
-    <div className="w-full flex flex-row pt-6">
-      {/* Filter Side Navigation */}
-      <div className="side-nav w-1/5 sticky">
-        <Filter config={config} />
-      </div>
-
-      {/* Main Content */}
-      <div className="main w-full h-max overflow-auto">
-        <SearchBar label="Search for an open problem" />
-        <Statbar className="statbar" />
-        <ProblemsInterface />
-      </div>
-
-      {/* Modal */}
-      <Modal open={modalState} close={modalCloseHandler}>
-        <SubmissionModalContent
-          questionDetails={questionDetails}
-          close={modalCloseHandler}
-        />
-      </Modal>
-    </div>
+    <Stack>
+      <Header />
+      <OpenProblemList openProblems={openProblems} loading={loading} />
+    </Stack>
   );
 }
 
