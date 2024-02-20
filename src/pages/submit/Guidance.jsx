9c399cbe4ea28e33774d5d617342149a306440d1
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-20 10:07:25 +0000

    Create Guidance.jsx

---
+++ src/pages/submit/Guidance.jsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+import guidance from "../../assets/guidance/submissionGuidance.json";
+
+// boxStyles for sx because using system props sends warning to console.
+const stackStyles = {
+  borderColor: "primary.main",
+};
+
+function Guidance() {
+  return (
+    <Paper variant="outlined">
+      <Stack
+        sx={stackStyles}
+        borderColor="primary.main"
+        padding={2}
+        alignItems="center"
+      >
+        <Typography variant="h5"> Writing a good open problem</Typography>
+        <Box className="guidance-description" padding={1}>
+          <Typography variant="subtitle1" sx={{ textDecoration: "underline" }}>
+            All open problems are welcome but we recommend following these
+            guidelines:
+          </Typography>
+          <List>
+            {guidance.map((pointer) => (
+              <ListItem key={pointer}>
+                <ListItemText primary={pointer} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Stack>
+    </Paper>
+  );
+}
+
+export default Guidance;
