5df28de22b644526883a6afaeb4ebbd2ebb858b9
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-20 18:57:05 +0000

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { grey } from "@mui/material/colors";
+
 import DefaultMargin from "./defaultMargin";
 import Footer from "./footer";
 import Header from "./header";
@@ -8,7 +10,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <DefaultMargin>
+      <DefaultMargin bgcolor={grey[100]}>
         <Outlet />
       </DefaultMargin>
       <Footer />
