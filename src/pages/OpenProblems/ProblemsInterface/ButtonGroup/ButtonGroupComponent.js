c3880059faf880526ef55a3d0f2be15d033d93c7
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-22 14:09:06 +0000

    Add report button

--- src/pages/OpenProblems/ProblemsInterface/ButtonGroup/ButtonGroupComponent.js
+++ src/pages/OpenProblems/ProblemsInterface/ButtonGroup/ButtonGroupComponent.js
@@ -1,20 +1,24 @@
+import React, { useEffect, useState } from "react";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import FlagIcon from "@mui/icons-material/Flag";
 import { Button, Tooltip } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { formActions } from "../../../../state/Question/questionFormSlice";
 import { HashLink } from "react-router-hash-link";
 import apiSubmissions from "../../../../api/apiSubmissions";
-import { useEffect, useState } from "react";
+import FeedbackForm from "../../feeback";
+
 function ButtonGroupComponent(props) {
   const { problem_id: problemId, title } = props.problem;
-  console.log(problemId);
+
   const isRoot = props.isRoot;
   const dispatch = useDispatch();
 
   // State for post counts
   const [counts, setCounts] = useState(0);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     async function getSubmissionCount() {
@@ -34,24 +38,38 @@ function ButtonGroupComponent(props) {
     );
   };
 
+  const closeHandler = () => {
+    setOpenDialog(false);
+  };
+
   return (
     <>
       <ButtonGroup size="small" variant="outlined">
         {isRoot && (
           <Tooltip title="view solutions">
-            <Button sx={{ fontsize: 10 }}>
+            <Button sx={{ fontsize: 10 }} startIcon={<ModeCommentIcon />}>
               <HashLink smooth to={`${problemId}#researchProposals`}>
-                <ModeCommentIcon fontSize="small" /> {counts} Solutions
+                {counts} Solutions
               </HashLink>
             </Button>
           </Tooltip>
         )}
         <Tooltip title="Add a connected problem">
-          <Button onClick={onClickHandlerForm} sx={{ fontSize: 10 }}>
-            <AddBoxIcon fontSize="small" /> Add Problem
+          <Button onClick={onClickHandlerForm} startIcon={<AddBoxIcon />}>
+            Add Problem
+          </Button>
+        </Tooltip>
+        <Tooltip>
+          <Button
+            color="error"
+            startIcon={<FlagIcon />}
+            onClick={() => setOpenDialog(true)}
+          >
+            Report
           </Button>
         </Tooltip>
       </ButtonGroup>
+      <FeedbackForm open={openDialog} onClose={closeHandler} />
     </>
   );
 }
