2cc2c9195d92367333ec80520bff0af924cd5b46
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-23 15:52:44 +0000

    Refactor

--- src/pages/OpenProblems/ProblemsInterface/ProblemsInterface.js
+++ src/pages/OpenProblems/ProblemsInterface/ProblemsInterface.js
@@ -1,32 +1,49 @@
 import { useSelector } from "react-redux";
-import View from "./View";
+import { Stack } from "@mui/system";
 import QuestionForm from "../Form/QuestionForm";
 import SortingInputs from "./Sorting/SortingInputs";
-function ProblemsInterface() {
-  const formState = useSelector((state) => state.form);
-  // const viewWidth = useSelector((state) => state.question.viewWidth);
-  return (
-    <div className="flex flex-col">
-      {!formState.submitFormOpen && (
-        <div className="sort-problems flex flex-row items-center justify-center px-4 pt-2">
-          <SortingInputs />
-        </div>
-      )}
+import MuiListComponent from "./List/MuiListComponent";
+import { List } from "@mui/material";
 
-      <div className="questions-container px-4 py-2">
-        {formState.submitFormOpen ? (
-          <QuestionForm
-            parent={
-              formState.chosenParent
-                ? formState.formDetails.parentTitle
-                : "None"
-            }
-          />
-        ) : (
-          <View></View>
-        )}
-      </div>
-    </div>
+function ProblemList() {
+  const allProblems = useSelector((state) => state.question.allProblems);
+  const filteredQuestions = useSelector(
+    (state) => state.question.filteredResults
+  );
+  // Ensure filteredQuestions and allProblems are arrays before mapping
+  const problemsToRender = filteredQuestions || allProblems || [];
+
+  return (
+    <List>
+      {problemsToRender.map((problem) => (
+        <MuiListComponent key={problem.problem_id} problem={problem} />
+      ))}
+    </List>
+  );
+}
+
+function ProblemsInterface({ error }) {
+  const formState = useSelector((state) => state.form);
+  return (
+    <>
+      {formState.submitFormOpen ? (
+        <QuestionForm
+          parent={
+            formState.chosenParent ? formState.formDetails.parentTitle : "None"
+          }
+        />
+      ) : (
+        <Stack
+          direction="column"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <SortingInputs />
+          <ProblemList />
+        </Stack>
+      )}
+    </>
   );
 }
 
