69ec0e00eeda25aa31f1a89535411b6385d8b0da
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-02-23 15:52:16 +0000

    Update OpenProblems.js
    Changed the way we retrieve problem list as it gave a long loading time.

--- src/pages/OpenProblems/OpenProblems.js
+++ src/pages/OpenProblems/OpenProblems.js
@@ -1,30 +1,50 @@
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Grid, Typography } from "@mui/material";
+
 import { questionActions } from "../../state/Question/questionSlice";
 import SubmissionModalContent from "../../components/UI/Modal/SubmissionModalContent";
 import Statbar from "./Statbar/Statbar";
 import SearchBar from "./SearchBar/SearchBar";
 import ProblemsInterface from "./ProblemsInterface/ProblemsInterface";
 import ModalT from "../../components/UI/Modal/Modal";
-import { useEffect } from "react";
-import { getProblems } from "../../utils/functions/getOpenProblems";
+import apiProblems from "../../api/apiProblems";
 
 function OpenProblems() {
+  const [error, setError] = useState(false);
   const modalState = useSelector((state) => state.question.modalOpen);
   const questionDetails = useSelector((state) => state.question.modalDetails);
+  const sorting = useSelector((state) => state.question.filters.sorting);
   const dispatch = useDispatch();
   const modalCloseHandler = () => {
     dispatch(questionActions.toggleModalClose());
   };
+
   useEffect(() => {
-    getProblems(dispatch);
-  });
-  // Set openProblems
+    const SORTING_OBJ = {
+      latest: apiProblems.getAllProblems,
+      top: apiProblems.sortedDescendantsDescending,
+      answered: apiProblems.sortedSubmissionAnswered,
+      root: apiProblems.getRootProblems,
+    };
+    async function getOpenProblems() {
+      try {
+        const apiCall = SORTING_OBJ[sorting];
+        const { data } = await apiCall();
+        dispatch(questionActions.setState({ key: "allProblems", value: data }));
+      } catch (error) {
+        setError(error);
+      }
+      setError(false);
+    }
+    getOpenProblems();
+  }, [sorting, dispatch]);
 
   return (
-    <div className="overflow-auto">
-      <div className="py-2 pb-6 text-center text-sm md:text-lg">
-        <p>
-          <strong>
+    <>
+      <Grid container direction="column" spacing={2} width="100%">
+        <Grid item xs={12}>
+          <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
             Our initial list of open problems in ageing and longevity science
             and their interconnected aspects organised in a hierarchical manner.
             Each problem of interest can represent a broader topic where you can
@@ -32,27 +52,30 @@ function OpenProblems() {
             areas of the topic. We encourage you to contribute your open
             problems to aid in refining our database and effectively
             categorizing these scientific challenges.
-          </strong>{" "}
-        </p>
-      </div>
-      <div>
-        <p className="pb-2 text-sm md:text-base">
-          {" "}
-          If you want to add an open problem that falls under the problems
-          provided, select the open problem and click the "add" button.
-          Otherwise use the submit open problem button in the bar below.
-        </p>
-      </div>
-      <SearchBar />
-      <Statbar className="statbar" />
-      <ProblemsInterface />
+          </Typography>
+        </Grid>
+        <Grid item xs={12} pt={2}>
+          <Typography className="pb-2 text-sm md:text-base">
+            If you want to add an open problem that falls under the problems
+            provided, select the open problem and click the "add" button.
+            Otherwise use the submit open problem button in the bar below.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <SearchBar />
+          <Statbar className="statbar" />
+        </Grid>
+        <Grid item xs={12}>
+          <ProblemsInterface error={error} />
+        </Grid>
+      </Grid>
       <ModalT open={modalState} close={modalCloseHandler}>
         <SubmissionModalContent
           questionDetails={questionDetails}
           close={modalCloseHandler}
         />
       </ModalT>
-    </div>
+    </>
   );
 }
 export default OpenProblems;
