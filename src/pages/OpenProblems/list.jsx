701878e1923cc0fa81460bcdc8aee4b656328c87
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-01 15:59:55 +0000

    Changed card layout.

--- src/pages/OpenProblems/list.jsx
+++ src/pages/OpenProblems/list.jsx
@@ -1,4 +1,4 @@
-import EditIcon from "@mui/icons-material/Edit";
+import FlagIcon from "@mui/icons-material/Flag";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import {
@@ -17,15 +17,16 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import apiAnnotations from "../../api/apiAnnotations";
 import { OpenProblemsContext } from "../../context/context";
 import extractAnnotationInformation from "../../utils/functions/extractAnnotationInformation";
 import useGetApi from "../../utils/hooks/useApi";
+import ReportForm from "./report";
 
-function OpenProblemCard({ openProblem, contact }) {
+function OpenProblemCard({ openProblem, contact, setReportOpen }) {
   const { title, description, problem_id: id } = openProblem ?? "";
   const { first_name: firstName, last_name: lastName } = contact ?? "";
   const { apiData, error } = useGetApi(
@@ -60,6 +61,11 @@ function OpenProblemCard({ openProblem, contact }) {
       <CardContent>
         <Grid container direction="column" spacing={2} padding={4}>
           <Grid item>
+            <Typography variant="subtitle2">
+              Posted by: {firstName} {lastName}
+            </Typography>
+          </Grid>
+          <Grid item>
             <Link component={RouterLink} to={`./${id}`} underline="hover">
               <Typography variant="h6">{title}</Typography>
             </Link>
@@ -88,23 +94,25 @@ function OpenProblemCard({ openProblem, contact }) {
                 ))}
             </Stack>
           </Grid>
-
-          <Grid item>
-            <Typography variant="subtitle2">
-              Posted by: {firstName} {lastName}
-            </Typography>
-          </Grid>
           <Grid item container justifyContent="center" xs={12}>
             <ButtonGroup>
               <Button
                 size="small"
                 startIcon={<ModeCommentIcon />}
                 color="primary"
+                component={RouterLink}
+                to={`./${id}`}
               >
                 0 Posts
               </Button>
               <Button startIcon={<VisibilityIcon />}>Follow</Button>
-              <Button startIcon={<EditIcon />}> Edit</Button>
+              <Button
+                startIcon={<FlagIcon />}
+                color="error"
+                onClick={() => setReportOpen(true)}
+              >
+                Report
+              </Button>
             </ButtonGroup>
           </Grid>
         </Grid>
@@ -124,6 +132,7 @@ function ListItemComponent({ id, title }) {
     </Paper>
   );
 }
+
 function calculatePagination(count, view) {
   const dividedBy = view === "card" ? 10 : 20;
   return Math.ceil(count / dividedBy);
@@ -132,6 +141,7 @@ function OpenProblemList({ openProblems }) {
   const { state, dispatch } = useContext(OpenProblemsContext);
   const { view } = state;
 
+  const [reportOpen, setReportOpen] = useState(false);
   const handlePageChange = (event, newPage) => {
     // Dispatch an action to update the page
     dispatch({ type: "setPage", payload: { page: newPage } });
@@ -145,14 +155,22 @@ function OpenProblemList({ openProblems }) {
   if (view === "card") {
     return (
       <Stack spacing={2} py={4} alignItems="center">
+        <Pagination
+          page={state.page}
+          count={paginationCount}
+          onChange={handlePageChange}
+          size="large"
+        />
         {openProblems &&
           openProblems.map((openProblem) => (
             <OpenProblemCard
               key={openProblem.id}
               openProblem={openProblem}
               contact={openProblem.contact}
+              setReportOpen={setReportOpen}
             />
           ))}
+        <ReportForm open={reportOpen} setOpen={setReportOpen} />
         <Pagination
           count={paginationCount}
           onChange={handlePageChange}
@@ -175,7 +193,12 @@ function OpenProblemList({ openProblems }) {
             ))}
         </Stack>
       </List>
-      <Pagination count={10} onChange={handlePageChange} size="large" />
+      <Pagination
+        page={state.page}
+        count={10}
+        onChange={handlePageChange}
+        size="large"
+      />
     </Stack>
   );
 }
