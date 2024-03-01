dddb8cadd46d0621ddb5cc6b9e3bf4edb7d7ae40
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-01 12:30:12 +0000

    Remove status and include contributor name

--- src/pages/OpenProblems/card.jsx
+++ src/pages/OpenProblems/card.jsx
@@ -1,27 +1,63 @@
-import React from "react";
-import { HashLink as Link } from "react-router-hash-link";
+import EditIcon from "@mui/icons-material/Edit";
+import ModeCommentIcon from "@mui/icons-material/ModeComment";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import {
+  Button,
+  ButtonGroup,
   Card,
   CardContent,
-  Typography,
-  Grid,
-  ButtonGroup,
-  Button,
   Chip,
+  Grid,
+  Link,
+  Stack,
+  Typography,
 } from "@mui/material";
-import ModeCommentIcon from "@mui/icons-material/ModeComment";
+import React, { useMemo } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import apiAnnotations from "../../api/apiAnnotations";
+import extractAnnotationInformation from "../../utils/functions/extractAnnotationInformation";
+import useGetApi from "../../utils/hooks/useApi";
 
-function OpenProblemCard({ openProblem }) {
+const TitleLink = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} {...props} />
+));
+
+function OpenProblemCard({ openProblem, contact }) {
   const { title, description, problem_id: id } = openProblem ?? "";
+  const { first_name: firstName, last_name: lastName } = contact ?? "";
+  const { apiData, error } = useGetApi(
+    apiAnnotations.getAnnotationsForProblem,
+    {
+      all: true,
+      problemId: id,
+      fields: ["compound", "subject", "gene", "species"],
+    }
+  );
+  const annotations = useMemo(() => {
+    // The api returns a nested structure of all annotations relating to an open problem we need to flatten it first so its easy to display
+    if (apiData && !error) {
+      const flattenedAnnotations = Object.entries(apiData).flatMap(
+        ([key, values]) => values.map((value) => ({ [key]: value[key] }))
+      );
+      if (flattenedAnnotations.length === 0) return [];
+
+      const formattedAnnotations = flattenedAnnotations.map((annotation) => {
+        const category = Object.keys(annotation)[0];
+        const annotationObject = Object.values(annotation)[0];
+        return extractAnnotationInformation(annotationObject, category);
+      });
+
+      return formattedAnnotations;
+    }
+    return [];
+  }, [id, apiData]);
+
   return (
     <Card sx={{ width: "100%" }}>
       <CardContent>
         <Grid container direction="column" spacing={2} padding={4}>
           <Grid item>
-            <Typography variant="subtitle2"> Posted by: - </Typography>
-          </Grid>
-          <Grid item>
-            <Link to={`/open-problems/${id}`}>
+            <Link component={TitleLink} to={`open-problems/${id}`}>
               <Typography variant="h6"> {title} </Typography>
             </Link>
           </Grid>
@@ -30,25 +66,43 @@ function OpenProblemCard({ openProblem }) {
               variant="body1"
               sx={{
                 display: "-webkit-box",
-                WebkitLineClamp: "8",
+                WebkitLineClamp: "3",
                 WebkitBoxOrient: "vertical",
               }}
             >
               {description}
             </Typography>
           </Grid>
-          <Grid item sx={12}>
-            <Chip label="test" color="primary" />
+          <Grid item sx={12} py={2}>
+            <Stack direction="row" spacing={2}>
+              {annotations &&
+                annotations.map((annotation) => (
+                  <Chip
+                    label={annotation.title}
+                    variant="outlined"
+                    color="primary"
+                  />
+                ))}
+            </Stack>
           </Grid>
+
           <Grid item>
+            <Typography variant="subtitle2">
+              {" "}
+              Posted by: {firstName} {lastName}
+            </Typography>
+          </Grid>
+          <Grid item container justifyContent="center" xs={12}>
             <ButtonGroup>
               <Button
                 size="small"
                 startIcon={<ModeCommentIcon />}
                 color="primary"
               >
-                Posts
+                0 Posts
               </Button>
+              <Button startIcon={<VisibilityIcon />}>Follow</Button>
+              <Button startIcon={<EditIcon />}> Edit</Button>
             </ButtonGroup>
           </Grid>
         </Grid>
