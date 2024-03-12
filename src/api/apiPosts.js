8e5697c75ea6a32e7da3065e96f390789dbff782
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-12 14:21:25 +0000

    Allow query params for api call.

--- src/api/apiPosts.js
+++ src/api/apiPosts.js
@@ -1,7 +1,7 @@
 import apiClient from "./apiClient";
 
 const apiPosts = {
-  forOpenProblem: ({ id }) => apiClient.get(`posts/${id}`),
+  forOpenProblem: ({ id, params }) => apiClient.get(`posts/${id}`, params),
   all: () => apiClient.get("posts/all"),
   submit: ({ data, problemId }) =>
     apiClient.post(`posts/${problemId}/submit`, data).catch((error) => error),
