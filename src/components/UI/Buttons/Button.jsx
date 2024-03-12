a39eee7a94475c7b679585d51097e6af96fbb106
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-12 12:38:20 +0000

    Delete unused files

--- src/components/UI/Buttons/Button.jsx
+++
@@ -1,5 +0,0 @@
-function Button() {
-  return <button type="button" />;
-}
-
-export default Button;
--- src/components/UI/Chip/Chip.jsx
+++
@@ -1,9 +0,0 @@
-function Chip({ children }) {
-  return (
-    <div className="chip border border-theme-blue w-fit rounded-3xl px-2 bg-white text-xs hover:bg-theme-blue hover:text-white">
-      {children}
-    </div>
-  );
-}
-
-export default Chip;
