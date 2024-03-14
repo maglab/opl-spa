8db448b50126e56da367e775bed85d29c1076381
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-14 10:36:06 +0000

    Create commentFormManager.jsx

---
+++ src/pages/OpenProblemDetails/commentFormManager.jsx
@@ -0,0 +1,24 @@
+import { Form, Formik } from "formik";
+import React from "react";
+import * as Yup from "yup";
+
+const initialValues = {
+  full_text: "",
+  alias: "",
+};
+
+const validationSchema = Yup.object().shape({
+  full_text: Yup.string().required("Comment must not be empty"),
+});
+
+export default function CommentFormManager({ onSubmitHandler, children }) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmitHandler}
+    >
+      <Form>{children}</Form>
+    </Formik>
+  );
+}
