cdba0d3adc78fcb26cc7ed694c48da523b72c7f1
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-03-18 10:59:58 +0000

    Include alias

--- src/pages/OpenProblemDetails/commentFormManager.jsx
+++ src/pages/OpenProblemDetails/commentFormManager.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import * as Yup from "yup";
 
 const initialValues = {
+  post: "",
   full_text: "",
   alias: "",
 };
 
 const validationSchema = Yup.object().shape({
   full_text: Yup.string().required("Comment must not be empty"),
+  alias: Yup.string().required("Alias / username must not be empty"),
 });
 
 export default function CommentFormManager({ onSubmitHandler, children }) {
