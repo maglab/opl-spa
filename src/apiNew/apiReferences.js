b74d6b3edb19017e1ca287414d21b743d32c7fe5
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-04-17 16:24:33 +0100

    Update apiReferences.js

--- src/apiNew/apiReferences.js
+++ src/apiNew/apiReferences.js
@@ -4,10 +4,7 @@ export const getReferenceForProblem = async ({ openProblemId }) =>
   apiClient.get(`open-problems/${openProblemId}/references`);
 
 export const verifyReference = async ({ type, value }) =>
-  apiClient.post(`posts/verify-reference`, { type, value });
-
-export const verifyReferences = async ({ references }) =>
-  apiClient.post("posts/verify-references", { references });
+  apiClient.post(`references/convert`, { type, value });
 
 export const getReferenceForSolution = async ({ submissionId }) =>
   apiClient.get(`posts/get/${submissionId}/submission/reference`);
