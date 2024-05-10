15e0250a0d125417dd0aaafce8d7c52a10e5cdeb
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2024-05-10 11:59:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.md", "**/*.html"],
+  assetsInclude: ["**/*.md"],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
